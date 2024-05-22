34f3b22b78c67325f57275db03df297bf1f30cc3
Marvin Fischer <marvinfischer@brivur.de>
2024-05-22 22:36:06 +0200

    init repo

---
+++ lib/ex_delivery_certificate.ex
@@ -0,0 +1,18 @@
+defmodule ExDeliveryCertificate do
+  @moduledoc """
+  Documentation for `ExDeliveryCertificate`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExDeliveryCertificate.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_delivery_certificate_test.exs
@@ -0,0 +1,8 @@
+defmodule ExDeliveryCertificateTest do
+  use ExUnit.Case
+  doctest ExDeliveryCertificate
+
+  test "greets the world" do
+    assert ExDeliveryCertificate.hello() == :world
+  end
+end
